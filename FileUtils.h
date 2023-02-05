7777bedc1e8e95cb615c1a2282dfcaae74eacc16
jbaron2025 <121137563+jbaron2025@users.noreply.github.com>
2023-02-04 22:01:50 -0800

    Create FileUtils.h

---
+++ FileUtils.h
@@ -0,0 +1,10 @@
+#ifndef FileReader_FILEUTILS_H
+#define FileReader_FILEUTILS_H
+
+#include <fstream>
+
+int readByte(std::ifstream &input);
+
+int readInt(std::ifstream &input);
+
+#endif // FileReader_FILEUTILS_H
